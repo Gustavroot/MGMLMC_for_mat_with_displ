9c4a382f5493618d80d9ab844d8f0359bf3c664d
Gustavroot <tavroot@gmail.com>
2023-05-15 11:33:32 +0200

    Re-inclusion of matrices #8

---
+++ matrices/D16/mats_slicer.m
@@ -0,0 +1,35 @@
+% cut matrices in half to be able to store on GitHub
+
+clc;clear;
+
+load D.mat;
+n = size(D,1);
+D_top = D(1:n/3,1:n);
+D_mid = D(n/3+1:2*n/3,1:n);
+D_low = D(2*n/3+1:n,1:n);
+save('D_top.mat','D_top');
+save('D_mid.mat','D_mid');
+save('D_low.mat','D_low');
+
+clc;clear;
+
+load V.mat;
+ni = size(V,1);
+nj = size(V,2);
+V_top = V(1:ni/3,1:nj);
+V_mid = V(ni/3+1:2*ni/3,1:nj);
+V_low = V(2*ni/3+1:ni,1:nj);
+save('V_top.mat','V_top');
+save('V_mid.mat','V_mid');
+save('V_low.mat','V_low');
+
+clc;clear;
+
+load Dreord.mat;
+n = size(Dreord,1);
+Dreord_top = Dreord(1:n/3,1:n);
+Dreord_mid = Dreord(n/3+1:2*n/3,1:n);
+Dreord_low = Dreord(2*n/3+1:n,1:n);
+save('Dreord_top.mat','Dreord_top');
+save('Dreord_mid.mat','Dreord_mid');
+save('Dreord_low.mat','Dreord_low');
